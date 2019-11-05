3d5a279cb575a9801b02975aef7f175d27474e60
Kaiser Ahmed <kaiser.ahmed89@gmail.com>
2019-11-06 04:39:00 +0600

    Update h5ai.sh

--- h5ai.sh
+++ h5ai.sh
@@ -2,13 +2,11 @@ cd /hdd && mkdir demo && cd demo
 wget -N --no-check-certificate https://release.larsjung.de/h5ai/h5ai-0.29.2.zip
 sudo apt install unzip -y && unzip -o h5ai-0.29.2.zip && rm h5ai-0.29.2.zip
 
-HOST_IP=$(hostname -I | cut -d' ' -f1)
-
 sudo touch /etc/nginx/sites-available/h5ai
 sudo bash -c 'cat > /etc/nginx/sites-available/h5ai << EOL 
 server {
     listen 80;
-    server_name $HOST_IP;
+    server_name HOST_IP;
     root /hdd/demo;
     index /_h5ai/public/index.php;
     limit_rate 1500k;
@@ -34,6 +32,9 @@ server {
     }
 }
 EOL'
+
+IP=$(hostname -I | cut -d' ' -f1)
+sed -i "s/HOST_IP/$IP/g" /etc/ssh/ssh_config
 
 sudo ln -s /etc/nginx/sites-available/h5ai /etc/nginx/sites-enabled/h5ai
 sudo chown -R www-data:www-data /hdd/demo
